c0441968ec7f40c870b800e10552489c3249c298
gtechzilla <okiomagerald@gmail.com>
2020-09-06 00:01:17 +0300

    created function to generate or influencer metrics

--- keys.py
+++
@@ -1,4 +0,0 @@
-api_key = "jWTfGrHUxfzgAyfh4WHWskUVi"
-api_secret_key = "Qnoh2PzAruvLHPdFT6wnBckYG91V1NVuzdy6D0cq5mrQvkhTeT"
-access_token = "3104444811-8VZEzuTP1KOUayu0Sez9L5RjhJwmTc6T1eBS3aj"
-access_token_secret = "8eHakf8Ypkx9HdbdGLJlU8MMsshfPfQuv3y0jxTpgeepW"
